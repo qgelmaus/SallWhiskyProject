9fd0899f5ba13680dc8bfecb4398c68e6f5df719
qgelmaus <qgelmaus@gmail.com>
2023-11-30 12:23:14 +0100

    Sall Whisky(2. Semester projekt) 1.0.1 added: MaltBatch, Medarbejder

--- src/application/model/Destillering.java
+++ src/application/model/Destillering.java
@@ -1,6 +1,24 @@
 package application.model;
 
+import java.time.LocalDate;
+
 public class Destillering {
+
+private LocalDate startDato;
+private LocalDate slutDato;
+private MaltBatch maltBatch;
+private Medarbejder medarbejder;
+private int væskeMængde;
+private String rygemateriale;
+private String kornSort;
+private String kommentar;
+
+public Destillering(LocalDate startDato, LocalDate slutDato, MaltBatch maltBatch, Medarbejder medarbejder){
+    this.startDato = startDato;
+    this.slutDato = slutDato;
+    this.maltBatch = maltBatch;
+    this.medarbejder = medarbejder;
+}
 
 
 
---
+++ src/application/model/Medarbejder.java
@@ -0,0 +1,8 @@
+package application.model;
+
+public class Medarbejder {
+    private String navn;
+    private String medarbejderNr;
+    private String telefonNr;
+
+}
