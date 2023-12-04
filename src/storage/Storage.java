9d8b82e0dc6dc8b256097a5eccde0abd2000443e
qgelmaus <qgelmaus@gmail.com>
2023-12-04 14:26:15 +0100

    sall whisky 2.0 Storage

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -1,7 +1,26 @@
 package storage;
 
+import application.model.Destillering;
+import application.model.Fad;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Storage {
     //TODO Lav storage
+    private List<Destillering> destillerings;
+    private List<Fad> fads;
+
+    public Storage(){
+        destillerings = new ArrayList<Destillering>();
+        fads = new ArrayList<Fad>();
+    }
+
+    //Alle gemte Destilleringer
+    public List<Destillering> getAllDestillerings(){
+        return new ArrayList<Destillering>(destillerings);
+    }
+
 
 
 
