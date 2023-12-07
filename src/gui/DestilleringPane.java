fc932915ca7d712ba7bf512fa6754933ba2a0d84
qgelmaus <qgelmaus@gmail.com>
2023-12-07 14:39:56 +0100

    sall whisky UI Fixes

--- src/gui/DestilleringPane.java
+++ src/gui/DestilleringPane.java
@@ -1,26 +1,19 @@
 package gui;
 
 import javafx.scene.layout.GridPane;
-
-import javax.swing.text.html.ListView;
+import javafx.scene.control.ListView;
 import java.awt.*;
 
-public class DestilleringPane {
-    public class DestilleringPane extends GridPane{
-        private TextField vælgDestillatTXT = new TextField();
-        private TextField vælgStartTXT = new TextField();
-        private TextField vælgSlutTXT = new TextField();
-        private TextField vælgBatchTXT = new TextField();
-        private TextField vælgVæskeTXT = new TextField();
-        private TextField vælgRøgTXT = new TextField();
-        private TextField vælgSortTXT = new TextField();
-        private TextField vælgMedarbjderTXT = new TextField();
-        private TextField bemærkningTXT = new TextField();
-        private ListView<destillatListLW> destillatListLW = new ListView();
 
-        private TextArea bemærkningTXA = new TextArea();
+public class DestilleringPane {
+
+
+
+
+
+
 
 
 
     }
-}
+
--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -3,38 +3,78 @@ package gui;
 //import application.controller.Controller;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TabPane.TabClosingPolicy;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 public class Gui extends Application {
-    /*
-private Controller controller;
+
+    private TextField vælgDestillatTXT, vælgStartTXT, vælgSlutTXT, vælgBatchTXT, vælgVæskeTXT, vælgRøgTXT, vælgSortTXT, vælgMedarbjderTXT, bemærkningTXT;
+    private ListView<String> destillatListLW;
+    private TextArea bemærkningTXA;
+    private GridPane gridPane;
+    private Button loginBtn;
 
 
     @Override
-    public void init() {
-        controller = Controller.getController;
-        controller.createSomeObjects;
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("LavWhisky");
 
-    } */
+        //Startside:
+        BorderPane startPane = new BorderPane();
+        Button loginBtn = new Button("Login");
 
-    @Override
-    public void start(Stage stage) {
-        stage.setTitle("Lav Whisky");
-        BorderPane pane = new BorderPane();
-        this.initContent(pane);
+        //Knap funktion:
+        loginBtn.setOnAction(event -> {
+            primaryStage.setScene(new Scene(gridPane, 800, 500));
+        });
 
-        Scene scene = new Scene(pane);
-        stage.setScene(scene);
-        stage.setHeight(500);
-        stage.setWidth(800);
-        stage.show();
+        startPane.setCenter(loginBtn);
+
+        Scene startScene = new Scene(startPane, 400, 200);
+
+        //Hovedside:
+        gridPane = new GridPane();
+        gridPane.setVgap(20);
+        gridPane.setHgap(20);
+
+        vælgDestillatTXT = new TextField();
+        vælgStartTXT = new TextField();
+        vælgSlutTXT = new TextField();
+        vælgBatchTXT = new TextField();
+        vælgVæskeTXT = new TextField();
+        vælgRøgTXT = new TextField();
+        vælgSortTXT = new TextField();
+        vælgMedarbjderTXT = new TextField();
+        bemærkningTXT = new TextField();
+        destillatListLW = new ListView<>();
+        bemærkningTXA = new TextArea();
+        vælgDestillatTXT.setText("Hej haukur");
+        gridPane.add(vælgDestillatTXT,0 ,0);
+
+        primaryStage.setScene(startScene);
+        primaryStage.setHeight(200);
+        primaryStage.setWidth(400);
+        primaryStage.show();
 
 
-        
+
+
+
+
+
+
+
 
         //TODO Tilføj knapper og andre ting til UI
     }
@@ -50,4 +90,7 @@ private Controller controller;
 
     }
 
+
+
 }
+
